ef68d5e03be5fbd266464ecceef19e6266fd47c4
bzzdenchik <83644351+bzzdenchik@users.noreply.github.com>
2023-07-19 21:10:08 +0600

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,18 @@
+from functools import *
+
+def moves(h):
+    a,b = h
+    return (a+1,b),(a+2,b),(a*2,b),(a,b+1),(a,b+2),(a,b*2)
+@lru_cache(None)
+def game(h):
+    a,b = h
+    if a+b>71 and (a+b)%3!=0:
+        return "W"
+    if any(game(i)=="W" for i in moves(h)): return "P1"
+    if all(game(i)=="P1" for i in moves(h)): return "V1"
+    if any(game(i)=="V1" for i in moves(h)): return "P2"
+    if all(game(i)=="P2" or game(i) == "P1" for i in moves(h)): return "V2"
+for s in range(1,69+1):
+    if game((2,s)) == "P2":
+        print(s, game((2, s)))
+    
